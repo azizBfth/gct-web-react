f7222aff84bd3dde1c8502fef8303d0c96b485f0
azizBfth <boufath.aziz83@gmail.com>
2023-05-08 15:46:48 +0100

    redesign

---
+++ src/pages/simulation/message.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+export default function Message(){
+
+
+return <>
+
+<h1>GCT MESSAGE CHECK</h1>
+</>
+
+
+}
--- src/pages/simulation/simulation.js
+++ src/pages/simulation/simulation.js
@@ -4,14 +4,15 @@ import Icons2 from "../../resources/images/icons2.jpeg";
 
 import "./simulation.css";
 import Header from "./Header";
+import Message from "./message";
 export default function Simulation() {
   return (
     <>
       <Header />
       <main
         style={{
-          display: "flex",
-          flexDirection: "column",
+          //  display: "flex",
+          //flexDirection: "column",
           height: "100%",
           backgroundColor: "#fefee2",
           color: "blue",
@@ -20,20 +21,19 @@ export default function Simulation() {
         <div
           style={{
             display: "flex",
-
             height: "40%",
             color: "green",
           }}
         >
           <div
             style={{
-              height: "100%",
+              height: "60%",
               width: "20%",
             }}
           ></div>
           <div
             style={{
-              height: "100%",
+              height: "60%",
               width: "60%",
               backgroundColor: "green",
             }}
@@ -54,15 +54,50 @@ export default function Simulation() {
             </div>
             <div
               style={{
+                display: "flex",
                 height: "70%",
                 width: "100%",
-                backgroundColor: "green",
+                // backgroundColor: "red",
               }}
-            ></div>
+            >
+              <div
+                style={{
+                  height: "100%",
+                  width: "20%",
+                  backgroundColor: "black",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                }}
+              >
+                <h1
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-around",
+                  }}
+                >
+                  29°C
+                </h1>
+              </div>
+              <div
+                style={{
+                  height: "100%",
+                  width: "80%",
+                  backgroundColor: "green",
+                  color: "white",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                }}
+              >
+                <Message />
+              </div>
+            </div>
           </div>
           <div
             style={{
-              height: "100%",
+              height: "60%",
               width: "20%",
             }}
           >
@@ -79,8 +114,24 @@ export default function Simulation() {
                 color: "blue",
               }}
             >
-              <h2>رسالة اليوم</h2>
-              <h2>MESSAGE DE JOUR</h2>
+              <h2
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                }}
+              >
+                رسالة اليوم
+              </h2>
+              <h2
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                }}
+              >
+                MESSAGE DE JOUR
+              </h2>
             </div>
           </div>
         </div>
@@ -100,37 +151,93 @@ export default function Simulation() {
               color: "blue",
             }}
           >
-            <h2>عدد الايام بدون حوادث عمل</h2>
-            <h2>Nombre des jours sansaccident de travail</h2>
+            <h2
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
+              }}
+            >
+              عدد الايام بدون حوادث عمل
+            </h2>
+            <h2
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
+              }}
+            >
+              Nombre des jours sansaccident de travail
+            </h2>
             <div
               style={{
                 height: "20%",
                 width: "60%",
                 backgroundColor: "grey",
-                color: "blue",
+                color: "white",
+                margin: "auto",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
               }}
-            ></div>
-            <img alt="" className="logo" src={Icons2} />
+            >
+              <h1
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                }}
+              >
+                61
+              </h1>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
+              }}
+            >
+              <img alt="" className="logo" src={Icons2} />
+            </div>
           </div>
           <div
             style={{
               height: "100%",
               width: "40%",
+              display:"flex",
+              alignItems:"center",
+              justifyContent:"space-around"
             }}
           >
             <div
               style={{
-                height: "70%",
+                height: "50%",
                 width: "70%",
                 backgroundColor: "red",
                 border: "5px solid white",
-                margin: "15%",
-                
-                
+                color: "white",
               }}
             >
-              <h2>إرتداء وسائل الوقاية إجباري</h2>
-              <h2>PORTER VOS EQUIPEMENTS DE PROTECTION</h2>
+              <h2
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                }}
+              >
+                إرتداء وسائل الوقاية إجباري
+              </h2>
+              <div
+          
+              >
+                <h2
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "space-around",
+                      }}>PORTER VOS EQUIPEMENTS DE PROTECTION</h2>
+              </div>
             </div>
           </div>
           <div
@@ -141,17 +248,55 @@ export default function Simulation() {
               color: "blue",
             }}
           >
-            <h2>العدد الجملي لحوادث الشغل</h2>
-            <h2>Nombre total d'accident de travail</h2>
+            <h2
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
+              }}
+            >
+              العدد الجملي لحوادث الشغل
+            </h2>
+            <h2
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
+              }}
+            >
+              Nombre total d'accident de travail
+            </h2>
             <div
               style={{
                 height: "20%",
                 width: "60%",
                 backgroundColor: "grey",
-                color: "blue",
+                color: "white",
+                margin: "auto",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
               }}
-            ></div>
-            <img alt="" className="logo" src={Icons1} />
+            >
+              <h1
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "space-around",
+                }}
+              >
+                61
+              </h1>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
+              }}
+            >
+              <img alt="" className="logo" src={Icons1} />
+            </div>
           </div>
         </div>
       </main>
